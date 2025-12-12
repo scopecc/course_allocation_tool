aaa89e7411e8631868f158bb501e9ee81d974c20
Vishal <santaclawsers@gmail.com>
2025-12-12 12:15:55 +0530

    fix: change selectitem labels

--- frontend/src/components/NewCourseModal.tsx
+++ frontend/src/components/NewCourseModal.tsx
@@ -153,9 +153,9 @@ export function NewCourseModal({ draftId, onSubmit }: { draftId: string, onSubmi
                       <SelectValue placeholder="YYYY" />
                     </SelectTrigger>
                     <SelectContent>
-                      <SelectItem value="2022">2025</SelectItem>
-                      <SelectItem value="2023">2025</SelectItem>
-                      <SelectItem value="2024">2025</SelectItem>
+                      <SelectItem value="2022">2022</SelectItem>
+                      <SelectItem value="2023">2023</SelectItem>
+                      <SelectItem value="2024">2024</SelectItem>
                       <SelectItem value="2025">2025</SelectItem>
                       <SelectItem value="2026">2026</SelectItem>
                       <SelectItem value="2027">2027</SelectItem>
